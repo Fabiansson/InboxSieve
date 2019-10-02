0a585fa698bbcd0ee05bc68b9b13a3dc73515c4e
Fabiansson <fabianzbinden@outlook.com>
2019-10-02 22:18:49 +0200

    changed gitignore and added client clonfig files

---
+++ client/src/config/firebase-config.jsx
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyCy56P9PEietwPjlH6SDcmzg0Sfq0VkKqc",
+    authDomain: "inboxsieve.firebaseapp.com",
+    databaseURL: "https://inboxsieve.firebaseio.com",
+    projectId: "inboxsieve",
+    storageBucket: "",
+    messagingSenderId: "873008654128",
+    appId: "1:873008654128:web:8cd461d4436fe5edb60af5"
+  };
+  
