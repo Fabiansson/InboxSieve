ddbae363865b226dfe74eff298ba8b06c9fdcced
Fabiansson <fabianzbinden@outlook.com>
2019-10-03 11:04:35 +0200

    extended req response for heroku

--- routes/api/inbox.js
+++ routes/api/inbox.js
@@ -5,6 +5,12 @@ const router = express.Router();
 // @desc    HTTP handler for messages from mailgun
 // @access  public
 router.post('/',(req, res) => {
+    console.log("REQ: ");
+    //console.log(req);
+    console.log("REQ BODY: ");
+    console.log(req.body);
+    console.log("REQ JSON: ");
+    console.log(JSON.stringify(req.body));
     const sender = req.body.sender;
     const recipient = req.body.recipient;
 
